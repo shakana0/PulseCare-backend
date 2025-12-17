0f165b2b9d15a99f9bdc8b705b622b6ebdfc4671
shakana0 <shakanajamal3@gmail.com>
2025-12-17 14:38:09 +0100

    chore: add project setup

---
+++ pulseCare.API/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
