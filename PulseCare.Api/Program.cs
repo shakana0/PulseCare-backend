d141d3632cca449419dc4d10502bcbb8e18f95f8
shakana0 <shakanajamal3@gmail.com>
2025-12-22 16:31:27 +0100

    feat: register dbcontext class and run seed on start

--- PulseCare.Api/Program.cs
+++ PulseCare.Api/Program.cs
@@ -1,10 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using PulseCare.API.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<PulseCareDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PulseCareDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
