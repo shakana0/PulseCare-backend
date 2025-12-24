acfac17efe38c614f59b3d0e72d8a368863b7669
shakana0 <shakanajamal3@gmail.com>
2025-12-24 13:08:25 +0100

    refactor: remove nullable field

--- PulseCare.Api/Data/Entities/Communication/Message.cs
+++ PulseCare.Api/Data/Entities/Communication/Message.cs
@@ -1,18 +1,19 @@
 using PulseCare.API.Data.Entities.Users;
 
 namespace PulseCare.API.Data.Entities.Communication;
+
 public class Message
-    {
-        public Guid Id { get; set; }
-        public Guid PatientId { get; set; }
-        public Patient? Patient { get; set; }
+{
+    public Guid Id { get; set; }
+    public Guid PatientId { get; set; }
+    public Patient? Patient { get; set; }
 
-        public Guid? DoctorId { get; set; }
-        public Doctor? Doctor { get; set; }
+    public Guid DoctorId { get; set; }
+    public Doctor? Doctor { get; set; }
 
-        public required string Subject { get; set; }
-        public required string Content { get; set; }
-        public DateTime Date { get; set; }
-        public bool Read { get; set; }
-        public bool FromPatient { get; set; }
-    }
+    public required string Subject { get; set; }
+    public required string Content { get; set; }
+    public DateTime Date { get; set; }
+    public bool Read { get; set; }
+    public bool FromPatient { get; set; }
+}
