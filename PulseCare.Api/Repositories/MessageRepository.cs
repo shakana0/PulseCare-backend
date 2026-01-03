ddb404f91c03fc84f590514fd88de4e91f89b307
shakana0 <shakanajamal3@gmail.com>
2026-01-03 20:38:58 +0100

    feat: add MessageRepository

--- PulseCare.Api/Repositories/MessageRepository.cs
+++ PulseCare.Api/Repositories/MessageRepository.cs
@@ -1,1 +1,53 @@
-public class MessageRepository : IMessageRepository { }
+using PulseCare.API.Context;
+using PulseCare.API.Data.Entities.Communication;
+using Microsoft.EntityFrameworkCore;
+public class MessageRepository : IMessageRepository
+{
+    private readonly PulseCareDbContext _context;
+
+    public MessageRepository(PulseCareDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Message> CreateMessageAsync(Guid conversationId, string subject, string content, bool fromPatient)
+    {
+        var message = new Message
+        {
+            Id = Guid.NewGuid(),
+            Subject = subject,
+            Content = content,
+            Date = DateTime.UtcNow,
+            Read = false,
+            FromPatient = fromPatient,
+            ConversationId = conversationId
+        };
+
+        _context.Messages.Add(message);
+        await _context.SaveChangesAsync();
+
+        return message;
+    }
+
+    public async Task<List<Message>> GetMessagesAsync(Guid conversationId)
+    {
+        return await _context.Messages
+            .Where(m => m.ConversationId == conversationId)
+            .OrderBy(m => m.Date)
+            .ToListAsync();
+    }
+
+    public async Task<bool> MarkMessageAsReadAsync(Guid messageId, Guid conversationId)
+    {
+        var message = await _context.Messages
+            .FirstOrDefaultAsync(m => m.Id == messageId && m.ConversationId == conversationId);
+
+        if (message is null)
+            return false;
+
+        message.Read = true;
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+}
