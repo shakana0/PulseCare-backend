0c1e9399caf46979d0685b4f66e572811a1e9ea0
Sindri Elfarsson <sindrielfarsson.dev@gmail.com>
2026-01-05 10:59:07 +0100

    migration: migrate changes

--- PulseCare.Api/Migrations/20260105093603_UserDataAdjustedToClerk.Designer.cs
+++ PulseCare.Api/Migrations/20260105093603_UserDataAdjustedToClerk.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PulseCare.API.Context;
 
@@ -11,9 +12,11 @@ using PulseCare.API.Context;
 namespace PulseCare.Api.Migrations
 {
     [DbContext(typeof(PulseCareDbContext))]
-    partial class PulseCareDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260105093603_UserDataAdjustedToClerk")]
+    partial class UserDataAdjustedToClerk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
